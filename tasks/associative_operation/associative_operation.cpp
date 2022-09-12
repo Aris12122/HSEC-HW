8c78af0aa3e05a7086a1e74fbb9fc59b59e4b9f8
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-12 21:43:37 +0300

    associative_operation solution v_1 added

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,5 +1,29 @@
 #include "associative_operation.h"
 
+bool InTable(size_t x, size_t n) {
+    return x < n;
+}
+
+
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
-    return false;
+    size_t n = table.size();
+    for (size_t x = 0; x < n; ++x) {
+        for (size_t y = 0; y < n; ++y) {
+            for (size_t z = 0; z < n; ++z) {
+                size_t left = table[x][y];
+                size_t right = table[y][z];
+
+                if (!InTable(left, n) || !InTable(right, n)) {
+                    return false;
+                }
+                left = table[left][z];
+                right = table[x][right];
+
+                if (left != right) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
 }
