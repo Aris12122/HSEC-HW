105d64c3d8740f9cb7d80385e4e1009183122cb4
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-12 21:59:59 +0300

    assoc_oper solution v_1 -> v_2 added check of n=m, remove func

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,19 +1,22 @@
 #include "associative_operation.h"
 
-bool InTable(size_t x, size_t n) {
-    return x < n;
-}
-
-
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
+    if (table.empty()) {
+        return true;
+    }
+
     size_t n = table.size();
+    if (table[0].size() != n) {
+        return false;
+    }
+
     for (size_t x = 0; x < n; ++x) {
         for (size_t y = 0; y < n; ++y) {
             for (size_t z = 0; z < n; ++z) {
                 size_t left = table[x][y];
                 size_t right = table[y][z];
 
-                if (!InTable(left, n) || !InTable(right, n)) {
+                if (left >= n || right >= n) {
                     return false;
                 }
                 left = table[left][z];
