4ea6b47b40c343b10992d3f515ee12dae146015b
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-13 19:50:25 +0300

    check for include string

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,26 +1,46 @@
+#include <string>
 #include "password.h"
 
+bool IsDigit(const char &c) {
+    return '0' <= c && c <= '9';
+}
+
+bool IsLower(const char &c) {
+    return 'a' <= c && c <= 'z';
+}
+
+bool IsUpper(const char &c) {
+    return 'A' <= c && c <= 'Z';
+}
+
+
 bool ValidatePassword(const std::string& password) {
-    if (password.length() < 8 || 14 < password.length()) {
+    int n = password.size();
+    if (n < 8 || n > 14) {
         return false;
     }
-    bool has_type[4];
-    std::fill(has_type, has_type + 4, false);
 
-    for (const char &c : password) {
-        if (static_cast<int>(c) < 33 || 126 < static_cast<int>(c)) {
+    bool has_type[4];
+    for (size_t i = 0; i < 4; ++i) {
+        has_type[i] = false;
+    }
+
+    for (const char& c : password) {
+        if (IsDigit(c)) {
+            has_type[0] = true;
+        } else if (IsLower(c)) {
+            has_type[1] = true;
+        } else if (IsUpper(c)) {
+            has_type[2] = true;
+        } else if (static_cast<char>(33) <= c && c <= static_cast<char>(126)) {
+            has_type[3] = true;
+        } else {
             return false;
         }
-
-        if (std::isdigit(c)) {
-            has_type[0] = true;
-        } else if (std::islower(c)) {
-            has_type[1] = true;
-        } else if (std::isupper(c)) {
-            has_type[2] = true;
-        } else {
-            has_type[3] = true;
-        }
     }
-    return std::count(has_type, has_type + 4, true) >= 3;
+    int cnt = 0;
+    for (size_t i = 0; i < 4; ++i) {
+        cnt += static_cast<int>(has_type[i]);
+    }
+    return cnt >= 3;
 }
