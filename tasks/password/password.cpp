4d2619034e57fcfe4f40166ecf2aee3cf2b31510
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-13 14:35:25 +0300

    password solution changed v1 -> v2: bool vars -> bool *

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -4,22 +4,20 @@ bool ValidatePassword(const std::string& password) {
     if (password.length() < 8 || 14 < password.length()) {
         return false;
     }
-    bool has_digits = false;
-    bool has_low_letters = false;
-    bool has_upp_letters = false;
-    bool has_others = false;
+    bool has_type[4];
+    std::fill(has_type, has_type + 4, false);
     for (const char &c : password) {
         if (std::isdigit(c)) {
-            has_digits = true;
+            has_type[0] = true;
         } else if (std::islower(c)) {
-            has_low_letters = true;
+            has_type[1] = true;
         } else if (std::isupper(c)) {
-            has_upp_letters = true;
+            has_type[2] = true;
         } else if (33 <= static_cast<int>(c) && static_cast<int>(c) <= 126) {
-            has_others = true;
+            has_type[3] = true;
         } else {
             return false;
         }
     }
-    return static_cast<int>(has_digits) + static_cast<int>(has_low_letters) + static_cast<int>(has_upp_letters) + static_cast<int>(has_others) >= 3;
+    return std::count(has_type, has_type + 4, true) >= 3;
 }
--- tasks/password/test.cpp
+++ tasks/password/test.cpp
@@ -3,8 +3,14 @@
 
 TEST_CASE("ValidatePassword") {
     REQUIRE(!ValidatePassword(""));
+    REQUIRE(!ValidatePassword("!@"));
+    REQUIRE(!ValidatePassword("!b@"));
+    REQUIRE(!ValidatePassword("1!2@"));
+
     REQUIRE(!ValidatePassword("qwerty"));
 
     REQUIRE(ValidatePassword("Aa1!Bb2@"));
+    REQUIRE(ValidatePassword("A1!B@AAA"));
+    REQUIRE(ValidatePassword("1!b2@1111"));
     REQUIRE(ValidatePassword("Aa!Aa!Aa!"));
 }
