28fbdd73d9de8d95f7e64f181ff0c1b5bba2d7b7
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-15 16:56:08 +0300

    removed unnecessary checks

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,28 +1,18 @@
 #include "associative_operation.h"
 
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
-    if (table.empty()) {
-        return true;
-    }
-
     size_t n = table.size();
-    if (table[0].size() != n) {
-        return false;
-    }
 
     for (size_t x = 0; x < n; ++x) {
         for (size_t y = 0; y < n; ++y) {
             for (size_t z = 0; z < n; ++z) {
-                size_t left = table[x][y];
-                size_t right = table[y][z];
+                size_t xy = table[x][y];
+                size_t yz = table[y][z];
 
-                if (left >= n || right >= n) {
-                    return false;
-                }
-                left = table[left][z];
-                right = table[x][right];
+                size_t xy_z = table[xy][z];
+                size_t x_yz = table[x][yz];
 
-                if (left != right) {
+                if (xy_z != x_yz) {
                     return false;
                 }
             }
