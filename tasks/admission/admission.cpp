304c8bd25cf33ad7e4a97c69269c52e23a75a56e
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-18 12:32:46 +0300

    First commit

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -1,5 +1,28 @@
 #include "admission.h"
 
-AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
+bool operator<(const Date& date1, const Date& date2) {
+    return std::tie(date1.year, date1.month, date1.day) <
+           std::tie(date2.year, date2.month, date2.day);
+}
+
+bool operator==(const Date& date1, const Date& date2) {
+    return std::tie(date1.year, date1.month, date1.day) ==
+           std::tie(date2.year, date2.month, date2.day);
+}
+
+bool operator<(const Student& student1, const Student& student2) {
+    return std::tie(student1.name, student1.birth_date) <
+           std::tie(student2.name, student2.birth_date);
+}
+
+bool operator<(const Applicant& applicant1, const Applicant& applicant2) {
+    return std::tie() <
+           std::tie();
+}
+
+
+
+AdmissionTable FillUniversities(const std::vector<University>& universities,const std::vector<Applicant>& applicants) {
+    std::sort(applicants.begin(), applicants.end());
     return {};
 }
