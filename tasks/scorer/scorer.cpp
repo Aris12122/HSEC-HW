881a5e2357afcb0eeaa14d327121dc4e44d1b108
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-23 13:54:25 +0300

    format fixed

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -5,25 +5,23 @@ struct TaskLast {
     time_t success = 0;
     time_t fail = 0;
     time_t merge_req_open = 0;
-    time_t merge_req_closed = 0;
+    time_t merge_req_closed = 1;
 };
 
-template<class T>
-bool AssignMax(T &a,const T &b) {
+template <class T>
+void AssignMax(T &a, const T &b) {
     if (a < b) {
         a = b;
-        return true;
     }
-    return false;
 }
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
-    std::unordered_map<StudentName, std::unordered_map<TaskName, TaskLast>> student_task_status;
+    std::map<std::pair<StudentName, TaskName>, TaskLast> student_task_status;
     for (const Event& event : events) {
         if (event.time > score_time) {
             continue;
         }
-        auto & task_last = student_task_status[event.student_name][event.task_name];
+        auto& task_last = student_task_status[{event.student_name,event.task_name}];
         switch (event.event_type) {
             case EventType::CheckFailed:
                 AssignMax(task_last.fail, event.time);
@@ -41,14 +39,12 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     }
 
     ScoreTable score_table;
-    for (const auto& [student_name, task_status]: student_task_status) {
-        for (const auto& [task_name, task_last] : task_status) {
-            if (task_last.success > task_last.fail) {
-                if (task_last.merge_req_closed >= task_last.merge_req_open) {
-                    score_table[student_name].insert(task_name);
-                }
+    for (const auto& [names, task_last] : student_task_status) {
+        if (task_last.success > task_last.fail) {
+            if (task_last.merge_req_closed > task_last.merge_req_open) {
+                score_table[names.first].insert(names.second);
             }
         }
     }
-    return score_table; // test
+    return score_table;
 }
