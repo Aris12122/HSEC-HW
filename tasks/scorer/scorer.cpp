f5f51fc960c28cf2418d9e4c3a70a6f047ca6c1c
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-23 13:36:33 +0300

    scorer solution v1 added

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,5 +1,54 @@
+#include <unordered_map>
 #include "scorer.h"
 
+struct TaskLast {
+    time_t success = 0;
+    time_t fail = 0;
+    time_t merge_req_open = 0;
+    time_t merge_req_closed = 0;
+};
+
+template<class T>
+bool AssignMax(T &a,const T &b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
-    return {}; // test
+    std::unordered_map<StudentName, std::unordered_map<TaskName, TaskLast>> student_task_status;
+    for (const Event& event : events) {
+        if (event.time > score_time) {
+            continue;
+        }
+        auto & task_last = student_task_status[event.student_name][event.task_name];
+        switch (event.event_type) {
+            case EventType::CheckFailed:
+                AssignMax(task_last.fail, event.time);
+                break;
+            case EventType::CheckSuccess:
+                AssignMax(task_last.success, event.time);
+                break;
+            case EventType::MergeRequestOpen:
+                AssignMax(task_last.merge_req_open, event.time);
+                break;
+            case EventType::MergeRequestClosed:
+                AssignMax(task_last.merge_req_closed, event.time);
+                break;
+        }
+    }
+
+    ScoreTable score_table;
+    for (const auto& [student_name, task_status]: student_task_status) {
+        for (const auto& [task_name, task_last] : task_status) {
+            if (task_last.success > task_last.fail) {
+                if (task_last.merge_req_closed >= task_last.merge_req_open) {
+                    score_table[student_name].insert(task_name);
+                }
+            }
+        }
+    }
+    return score_table; // test
 }
