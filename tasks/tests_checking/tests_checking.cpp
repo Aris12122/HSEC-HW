2d84fd6889ff79e098009394c1d1d25d686d03e3
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-23 14:18:34 +0300

    format fixed

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -4,7 +4,7 @@
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
     std::size_t students_number = student_actions.size();
-    std::deque <const std::string*> paper_list;
+    std::deque<const std::string*> paper_list;
     for (const StudentAction& action : student_actions) {
         switch (action.side) {
             case Side::Bottom:
@@ -15,7 +15,7 @@ std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student
                 break;
         }
     }
-    std::vector <const std::string*> paper_by_order;
+    std::vector<const std::string*> paper_by_order;
     paper_by_order.reserve(students_number);
     for (const std::string* name : paper_list) {
         paper_by_order.emplace_back(name);
