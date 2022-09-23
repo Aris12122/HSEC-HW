38e8407d0d1d1bd44bdce0ebccad64f6c227ba20
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-23 12:06:12 +0300

    admission solution added

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -16,13 +16,45 @@ bool operator<(const Student& student1, const Student& student2) {
 }
 
 bool operator<(const Applicant& applicant1, const Applicant& applicant2) {
-    return std::tie() <
-           std::tie();
+    return std::tie(applicant2.points, applicant1.student.birth_date, applicant1.student.name) <
+           std::tie(applicant1.points, applicant2.student.birth_date, applicant1.student.name);
 }
 
 
 
 AdmissionTable FillUniversities(const std::vector<University>& universities,const std::vector<Applicant>& applicants) {
-    std::sort(applicants.begin(), applicants.end());
-    return {};
+    std::vector<const Applicant*> applicants_pointers (applicants.size());
+    for (std::size_t i = 0; i < applicants_pointers.size(); ++i) {
+        applicants_pointers[i] = static_cast<const Applicant*>(&applicants[i]);
+    }
+    std::sort(applicants_pointers.begin(), applicants_pointers.end(), [](const Applicant* applicant1, const Applicant* applicant2) {
+        return *applicant1 < *applicant2;
+    });
+
+    AdmissionTable admission_table;
+    std::unordered_map<std::string, std::size_t> university_capacity;
+
+    for (const University& university : universities) {
+        university_capacity[university.name] = university.max_students;
+    }
+
+    for (const Applicant* applicant_ptr : applicants_pointers) {
+        const Student& student = applicant_ptr->student;
+        const std::vector<std::string>& wish_list = applicant_ptr->wish_list;
+
+        for (const std::string& priority_university : wish_list) {
+            auto & admission_list = admission_table[priority_university];
+            if (admission_list.size() < university_capacity[priority_university]) {
+                admission_list.emplace_back(static_cast<const Student*>(&student));
+                break;
+            }
+        }
+    }
+
+    for (auto &[university, student_list] : admission_table) {
+        std::sort(begin(student_list), end(student_list), [](const Student* student1, const Student* student2){
+            return *student1 < *student2;
+        });
+    }
+    return admission_table;
 }
