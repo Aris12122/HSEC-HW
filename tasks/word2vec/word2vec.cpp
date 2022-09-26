46b19ecc1d9193ff903d2f932247a0b80183bdcb
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-26 19:13:14 +0300

    solution v1 added

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -2,7 +2,28 @@
 
 #include <vector>
 
+int ScalarProduct(const std::vector<int>& v1, const std::vector<int>& v2) {
+    int scalar_product = 0;
+    for (std::size_t i = 0; i < v1.size(); ++i) {
+        scalar_product += v1[i] * v2[i];
+    }
+    return scalar_product;
+}
+
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
-    return {};
+
+    std::vector<std::string> closest_words;
+    int max_scalar_product = INT32_MIN;
+    for (std::size_t i = 1; i < words.size(); ++i) {
+        int scalar_product = ScalarProduct(vectors[0], vectors[i]);
+        if (scalar_product < max_scalar_product) {
+            continue;
+        } else if (scalar_product > max_scalar_product) {
+            max_scalar_product = scalar_product;
+            closest_words.clear();
+        }
+        closest_words.emplace_back(words[i]);
+    }
+    return closest_words;
 }
