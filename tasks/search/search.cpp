e5a97b64de41ee6d39a5c9520cc6d6ddacace894
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-26 21:00:19 +0300

    skip empty lines added

--- tasks/search/search.cpp
+++ tasks/search/search.cpp
@@ -19,7 +19,9 @@ std::vector<std::string_view> ParseText(const std::string_view& text) {
     do {
         const char* line_begin = std::next(line_end);
         line_end = std::find(line_begin, text.end(), '\n');
-        lines.emplace_back(text.substr(line_begin - text.begin(), line_end - line_begin));
+        if (std::find_if(line_begin, line_end, std::isalpha) != line_end) {
+            lines.emplace_back(text.substr(line_begin - text.begin(), line_end - line_begin));
+        }
     } while (line_end != text.end());
     return lines;
 }
