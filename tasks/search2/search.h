ead64bcb1d6f8de899da34b884ccc9ea2be48ed0
Mikhail Tropin <matropin_1@edu.hse.ru>
2022-09-29 23:25:31 +0300

    format fixed

--- tasks/search2/search.h
+++ tasks/search2/search.h
@@ -7,7 +7,7 @@ class SearchEngine {
 private:
     std::vector<std::string_view> lines_;
     std::vector<std::vector<std::string_view>> parsed_text_;
-    
+
 public:
     void BuildIndex(std::string_view text);
     std::vector<std::string_view> Search(std::string_view query, size_t results_count) const;
